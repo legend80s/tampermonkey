d0b80a2bacd223689b61327e664924a99e861c07
legend80s <liuchzong@qq.com>
2025-02-26 18:23:03 +0800

    add copy code btn

--- npm-helper.js
+++ npm-helper.js
@@ -1,71 +1,137 @@
 // ==UserScript==
-// @name         NpmHelper
+// @name         Npm Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  try to take over the world!
-// @author       You
+// @author       legend80s
 // @match        https://www.npmjs.com/package/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
-// @grant        GM_info
-// @grant        GM_addElement
+// @grant        none
 // ==/UserScript==
 
-// 1.0 add link for UNPKG and jsDelivr
-// 0.1 NpmPackageAutoInstaller
-
-(function() {
+// CHANGELOG
+// 2.0 insert cdn links
+// 1.0 初始化 add copy code btn
+(async function () {
   'use strict';
-  const { insertScript, createLogger, ready, $, $$ } = tampermonkeyUtils
 
-  const error = createLogger('error', GM_info)
-  const log = createLogger('log', GM_info)
-  const warn = createLogger('warn', GM_info)
+  // Your code here...
+  const {
+    $,
+    ready,
+    createLoggers,
+    time2Readable,
+    onUrlChange,
+    toast,
+    sleep,
+    // eslint-disable-next-line no-undef
+  } = tampermonkeyUtils;
+  const { insertScript, createLogger, $$ } = tampermonkeyUtils;
 
-  const hostSelector = '#main h2'
+  const { log, error, warn } = createLoggers(GM_info);
 
-  main()
+  main();
 
-  async function main() {
+  function main() {
+    init();
+    insertLinks();
+
+    // onUrlChange(() => init())
+  }
+
+  async function init() {
+    const begin = Date.now();
+    // Code here
+    $('#main').addEventListener('click', async (event) => {
+      await sleep(10);
+      console.log('e.target', event.target);
+
+      const preElement = $('#tabpanel-explore pre');
+      let el;
+      if (preElement?.contains(event.target)) {
+        el = preElement;
+      }
+      console.log('el', el);
+      if (!el) {
+        return;
+      }
+
+      const lineNumbers = el.querySelectorAll('.linenumber');
+      let display;
+      lineNumbers.forEach((line) => {
+        display ??= line.style.display;
+        line.style.display = 'none';
+      });
+      const code = el.innerText;
+      lineNumbers.forEach((line) => (line.style.display = display));
+
+      await navigator.clipboard.writeText(code);
+      toast('代码已复制 ✅');
+    });
+
+    log('🎉 耗时', time2Readable(begin, Date.now()));
+  }
+
+
+  async function insertLinks() {
     // '/package/console-next'
-    const matches = location.pathname.match(/^\/package\/([^/]+)$/)
+    const matches = location.pathname.match(/^\/package\/([^/]+)$/);
 
-    if (!matches) { return error('cannot found package name in location.pathname', location.pathname) }
+    if (!matches) {
+      return error(
+        'cannot found package name in location.pathname',
+        location.pathname
+      );
+    }
 
     const name = matches[1];
+    const hostSelector = '#main h2';
 
-    await ready(hostSelector)
+    await ready(hostSelector);
 
-    const host = $(hostSelector)
+    const host = $(hostSelector);
 
-    if (!host) { return error(`no element (${hostSelector}) found`); }
+    if (!host) {
+      return error(`no element (${hostSelector}) found`);
+    }
 
     insertCdnLink(host, name);
 
-    installNpmByUnpkg(host, name)
+    installNpmByUnpkg(host, name);
   }
 
   function insertCdnLink(host, name) {
-    const href = `https://cdn.jsdelivr.net/npm/${name}/`
+    const href = `https://cdn.jsdelivr.net/npm/${name}/`;
 
-   host.insertAdjacentHTML('beforeend', `<a class="f4 fw6 fl db pv1 ma1 red-500 link hover-black animate" href="${href}" style="color:#cb3837;margin: 0 1rem;" target="_blank">jsDelivr</a>`)
+    host.insertAdjacentHTML(
+      'beforeend',
+      `<a class="f4 fw6 fl db pv1 ma1 red-500 link hover-black animate" href="${href}" style="color:#cb3837;margin: 0 1rem;" target="_blank">jsDelivr</a>`
+    );
   }
 
   async function installNpmByUnpkg(host, name) {
-    const id = `npm-helper-mz`
-    host.insertAdjacentHTML('beforeend', `<button class="f4" id="${id}" style="/* border: 1px solid #cccccc; */">安装 ⚙</button>`)
+    const id = `npm-helper-mz`;
+    host.insertAdjacentHTML(
+      'beforeend',
+      `<button class="f4" id="${id}" style="/* border: 1px solid #cccccc; */">安装 ⚙</button>`
+    );
 
     await ready(`#${id}`);
-    const version = $$('h3').filter(h => h.textContent === 'Version')[0].nextSibling.textContent.trim();
+    const version = $$('h3')
+      .filter((h) => h.textContent === 'Version')[0]
+      .nextSibling.textContent.trim();
     let installed = false;
 
     $(`#${id}`).addEventListener('click', () => {
-      if (installed) { return warn(`${name}@${version} has already installed.`) }
+      if (installed) {
+        return warn(`${name}@${version} has already installed.`);
+      }
 
-      insertScript('https://unpkg.com/'+name+`@${version}`, GM_addElement);
+      insertScript('https://unpkg.com/' + name + `@${version}`, GM_addElement);
 
-      log(`${name}@${version} installed in your console.`)
+      log(`${name}@${version} installed in your console.`);
 
       installed = true;
-    })
+    });
   }
 })();
