d25d560c102aaae9a095140bedac0d8a7aa0e11b
legend80s <liuchzong@qq.com>
2025-04-28 14:14:16 +0800

    Update and rename replace-github.userscript.js to replace-link.userscript.js

--- replace-link.userscript.js
+++ replace-link.userscript.js
@@ -1,9 +1,13 @@
 // ==UserScript==
-// @name         替换网页 GitHub 地址
+// @name         替换 GitHub/Medium
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  try to take over the world!
+// @version      2.0
+// @description  replace github / medium links
 // @author       legend80s
+// @match        https://vite.dev
+// @match        https://umijs.org/*
+// @match        https://d.umijs.org/*
+// @match        https://2ality.com/*
 // @match        https://npmmirror.com/package/*
 // @match        https://4x-ant-design.antgroup.com/*
 // @match        https://ant-design.antgroup.com/*
@@ -12,12 +16,14 @@
 // @match        https://www.ruanyifeng.com/blog/*.html
 // @match        https://ant-design.antgroup.com/components/*
 
-// @homepage     https://github.com/legend80s/tampermonkey/blob/master/replace-github.userscript.js
+// @homepage     https://github.com/legend80s/tampermonkey/blob/master/replace-link.userscript.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=npmmirror.com
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
 // CHANGELOG
+// 2.0 支持 Medium
+// 1.2 add https://2ality.com/2025/02/typescript-esm-packages.html
 // 1.1 支持从多个github替换地址中选择一个速度最快的
 // 1.0 初始化
 (async function() {
@@ -50,13 +56,38 @@
   async function init() {
     const begin = Date.now();
     // Code here
+    await replaceMedium()
 
     await replace()
     await replace()
     sleep(1000)
     await replace()
+    //await replaceMedium()
 
     log('🎉 耗时', time2Readable(begin, Date.now()))
+  }
+
+  async function replaceMedium() {
+    const github = `https://medium.com`
+    const s = `a[href^="${github}"]`
+    await $Async(s)
+
+    log('medium list:', document.querySelectorAll(s).length)
+
+    const list = [...document.querySelectorAll(s)].filter(x => {
+      return !x.__replaced // && !!x.textContent
+    })
+    log('medium list:', list.length)
+
+    for (const item of list) {
+      const { url, tips } = await genMedium(item.href)
+
+      item.href = url
+      item.__replaced = true
+      // log('mark', item.textContent, item, item.__replaced)
+      item.title = (item.title || '') + url + ' ' + tips
+      item.insertAdjacentHTML('beforeEnd', `<span style="font-size: 68%;">${tips}</span>`)
+    }
   }
 
   async function replace() {
@@ -64,7 +95,7 @@
     const s = `a[href^="${github}"]`
     await $Async(s)
 
-    log('list1:', document.querySelectorAll(s).length)
+    log('github list:', document.querySelectorAll(s).length)
     const list = [...document.querySelectorAll(s)].filter(x => {
       return !x.__replaced // && !!x.textContent
     })
@@ -87,7 +118,7 @@
   const is200 = isSiteAccessible(GM)
 
   function getFastestReplacement(candidates) {
-/*     tampermonkeyUtils.gm = GM */
+    /*     tampermonkeyUtils.gm = GM */
     // console.log(tampermonkeyUtils.gm)
     const { resolve, reject, promise } = Promise.withResolvers()
     const rejectedUrls = []
@@ -101,6 +132,25 @@
     setTimeout(() => { reject({ rejectedUrls, msg: 'timeout' }) }, 3000)
 
     return promise
+  }
+
+  async function genMedium(url) {
+    // if we can detect if github is accessible
+    // log(url)
+    const urlInstance = new URL(url)
+
+    if (urlInstance.hostname !== 'medium.com') { return { url, tips: '' } }
+
+    let final = ''
+    let icon = ''
+
+    //console.time('getFastestReplacement')
+    const fast = 'https://readmedium.com'
+
+    final = url.replace(urlInstance.origin, fast)
+    icon = 'Ⓜ️'
+
+    return { url: final, tips: icon }
   }
 
   async function replaceGithub(url) {
@@ -124,8 +174,9 @@
     } else {
       //console.time('getFastestReplacement')
       const fast = await getFastestReplacement([
+        'https://dgithub.xyz',
         'https://git.homegu.com',
-        'https://kkgithub.com',
+        // 'https://kkgithub.com',
         'https://hgithub.xyz',
         'https://hub.whtrys.space'
       ])
